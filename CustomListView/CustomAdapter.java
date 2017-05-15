736feca5d0beeffb89280a627f7ad44be4f9384c
vintisachdeva <vintisachdeva17@gmail.com>
2017-05-15 14:55:39 +0530

    Add files via upload

---
+++ CustomListView/CustomAdapter.java
@@ -0,0 +1,34 @@
+package com.bmpl.customlistview;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class CustomAdapter extends BaseAdapter{
+
+
+    CustomAdapter(MainActivity mainActivity, String data[], int images[]){
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
